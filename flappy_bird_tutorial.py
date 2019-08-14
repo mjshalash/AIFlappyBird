2836904c4eae5925dd909a4d208505ffe0d2c87d
Malik Shalash <mjshalash@rci4u.com>
2019-08-14 14:48:41 -0400

    Added pipe class and finished bird class, still working on base class

--- flappy_bird_tutorial.py
+++ flappy_bird_tutorial.py
@@ -5,8 +5,8 @@ import os
 import random
 
 # Constants for Window Size
-WIN_WIDTH = 600
-WIN_WIDTH = 800
+WIN_WIDTH = 500
+WIN_HEIGHT = 800
 
 # Load Images
 # Load bird images and make 2x bigger
@@ -18,17 +18,14 @@ BIRD_IMGS = [
     pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
 
 # Load pipe images and make 2x bigger
-PIPE_IMG = [
-    pygame.transform.scale2x(pygame.image.load(
-        os.path.join("imgs", "pipe.png")))]
+PIPE_IMG = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("imgs", "pipe.png")))
 
-BASE_IMG = [
-    pygame.transform.scale2x(pygame.image.load(
-        os.path.join("imgs", "base.png")))]
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(
+    os.path.join("imgs", "base.png")))
 
-BG_IMG = [
-    pygame.transform.scale2x(pygame.image.load(
-        os.path.join("imgs", "bg.png")))]
+BG_IMG = pygame.transform.scale2x(pygame.image.load(
+    os.path.join("imgs", "bg.png")))
 
 
 # Bird Class
@@ -90,3 +87,160 @@ class Bird:
     def draw(self, win):
         # How many times has the game loop run
         self.img_count += 1
+
+        # Check what image to show based on image count
+        # This simulates the flapping animation (up then down)
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        # No flap if facing down
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+        # Rotate image around center (found on Stack Overflow)
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(
+            center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    # Collision handling
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+# Pipe Class
+class Pipe:
+    # Gap between pipes
+    GAP = 200
+    # Pipes moving backwards (remember the bird isnt moving, everything else is)
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0  # Will be assigned randomly later
+
+        # Keep track of where the top and bottom of pipe will be drawn
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+
+    # Set gap
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        # For top pipe, we need to do some math to set actual position
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    # Move pipes toward bird each frame
+    def move(self):
+        self.x -= self.VEL
+
+    # Draw Pipes
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    # Check collision with bird using masks (not just straight box colliders)
+    def collide(self, bird, win):
+        # Get pixels for bird
+        bird_mask = bird.get_mask
+
+        # Create masks for top and bottom pipes
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        # Calculate how far the masks are from each other
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        # Check if the masks collide
+        # (Returns none if no collision)
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True
+
+        return False
+
+
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    # Move base backwards to simulate movement
+    #
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+
+def draw_window(win, bird):
+    # Draw background/environment
+    win.blit(BG_IMG, (0, 0))
+
+    # Start bird's movement/flapping
+    bird.draw(win)
+    pygame.display.update()
+
+
+def main():
+    # Create bird object
+    bird = Bird(200, 200)
+
+    # Create pygame window
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+    # Establish clock so we can control how quick the loop loops
+    clock = pygame.time.Clock()
+
+    # Boolean to control game running or not
+    run = True
+
+    # Will loop every frame
+    while run:
+        clock.tick(30)
+
+        # Check for user events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # Move bird forward
+        bird.move()
+
+        # Draw the game window
+        draw_window(win, bird)
+
+    # If loop exits then quit game
+    pygame.quit()
+    quit()
+
+
+main()
